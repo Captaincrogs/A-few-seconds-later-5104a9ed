1c13faf70bc79eaf707fc57801e3850460f0471c
Captaincrogs <2003829@talnet.nl>
2019-11-28 10:25:44 +0100

    Uitwerking van de exercise

--- tijd.php
+++ tijd.php
@@ -1,13 +1,18 @@
 <?php
 
-echo strpos("ik hou van php, Ik hou ook van php!","php");{
-  
-}
-$lorem = (boolean) true;
-var_dump ($lorem);
-array_push($lorem);
-echo $lorem;
+
+
+readline ("wat is je tijd" );
+$seconds = "150";
+$minutes = floor($seconds/60);
+$secondsleft = $seconds%60;
+if($minutes<10)
+	$minutes = "0" . $minutes;
+if($secondsleft<10)
+	$secondsleft = "0" . $secondsleft;
+
+echo "$minutes:$secondsleft minutes";
 
 
 
- ?>
+?>
